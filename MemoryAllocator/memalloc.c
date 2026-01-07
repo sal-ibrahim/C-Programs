77b0e0b546a20045840b90fa8a01c86b16e92ccd
ib <ib.salat272@gmail.com>
2026-01-07 17:12:44 -0500

    implemented free function to a block can be released or reused

--- MemoryAllocator/memalloc.c
+++ MemoryAllocator/memalloc.c
@@ -18,8 +18,36 @@ typedef union header header_t;
 header_t *head = NULL, *tail = NULL;
 pthread_mutex_t global_malloc_lock;
 
-void free(void *block){
 
+void free(void *block){
+	header_t *header, *tmp;
+	void *programbreak;
+
+	if (!block)
+		return;
+	pthread_mutex_lock(&global_malloc_lock);
+	header = (header_t*)block - 1;
+
+	programbreak = sbrk(0);
+	if ((char*)block + header->s.size == programbreak) {
+		if (head == tail) {
+			head = tail = NULL;
+		} else {
+			tmp = head;
+			while (tmp) {
+				if(tmp->s.next == tail) {
+					tmp->s.next = NULL;
+					tail = tmp;
+				}
+				tmp = tmp->s.next;
+			}
+		}
+		sbrk(0 - sizeof(header_t) - header->s.size);
+		pthread_mutex_unlock(&global_malloc_lock);
+		return;
+	}
+	header->s.is_free = 1;
+	pthread_mutex_unlock(&global_malloc_lock);
 }
 
 void *calloc(size_t num, size_t nsize){
